9a1a7167f6a4e6f049a66a51006f0d1c1c9afd1b
miloshov <miloshov84@gmail.com>
2017-08-02 15:17:01 +0200

    2nd Commit  - Push Notifications

--- CompanyApp/CallUs.swift
+++ CompanyApp/CallUs.swift
@@ -7,3 +7,20 @@
 //
 
 import Foundation
+import UIKit
+
+// MARK: Custume function for calling specific number (this should be called from some "CallUs" button)
+
+@available(iOS 10.0, *)
+func callNumber(phoneNumber:String) {
+    
+    if let phoneCallURL = URL(string: "tel://\(phoneNumber)") {
+        
+        let application:UIApplication = UIApplication.shared
+        if (application.canOpenURL(phoneCallURL)) {
+            application.open(phoneCallURL, options: [:], completionHandler: nil)
+        }
+    }
+    
+}
+
